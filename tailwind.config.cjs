21eccdeae73081ffdf2e7fe9c4eec0cbc1e32168
Gilbert Zhang <gilbertzhang1208@gmail.com>
2023-08-29 19:36:06 -0400

    feat: add fade in animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./index.html", "./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
@@ -13,5 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
